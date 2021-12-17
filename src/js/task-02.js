cea2ea15b0c6c27487628571db04168e8b6dabdf
Vladislav220294 <vladislav22021994@gmail.com>
2021-12-17 20:31:40 +0300

    Добавляет 3 задачу

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsEl = document.querySelector('#ingredients');
+
+const a = ingredients.map(i => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.classList.add('item');
+  ingredientEl.textContent = i;
+
+  return ingredientEl;
+});
+ingredientsEl.append(...a);
