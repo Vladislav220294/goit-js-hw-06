33ed796a183cdcfed4d262f443dddaf0254cb43b
Vladislav220294 <vladislav22021994@gmail.com>
2021-12-19 18:18:14 +0300

    добавляет 9 задачу

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector('body');
+const btnActionEl = document.querySelector('.change-color');
+const textColorEl = document.querySelector('.color');
+
+btnActionEl.addEventListener('click', onActiveBtn);
+
+function onActiveBtn() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textColorEl.textContent = getRandomHexColor();
+}
