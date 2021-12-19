8bea72b33173a5b6180ab28ae62dc7dee1ef431b
Vladislav220294 <vladislav22021994@gmail.com>
2021-12-19 17:48:27 +0300

    добавляет 8 задачу

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (
+    event.currentTarget.elements.email.value === '' ||
+    event.currentTarget.elements.password.value === ''
+  ) {
+    return alert('Все поля должны быть заполнены');
+  }
+  const mail = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  const formData = { mail, password };
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
